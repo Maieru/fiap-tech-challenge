11c6af303ff6f88150245b7dd08e821de3490303
Maieru <soljoaomaieru@hotmail.com>
2026-04-11 17:11:33 -0300

    Remoção da pasta migrations do scanner do sonar

--- src/sonarscan.bat
+++ src/sonarscan.bat
@@ -1,4 +1,4 @@
-dotnet sonarscanner begin /k:"FiapFase1" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_c65ca74097e6cb83cf48141072f915825b122f7a" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
+dotnet sonarscanner begin /k:"FiapFase1" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_c65ca74097e6cb83cf48141072f915825b122f7a" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml   /d:sonar.exclusions="**/Migrations/**"
 
 
 dotnet build .\TechChallengeFase1.slnx
