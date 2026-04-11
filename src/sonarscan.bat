fae4a97959bd0431214ec007bc046e5e0ba85600
Maieru <soljoaomaieru@hotmail.com>
2026-04-11 11:04:13 -0300

    Melhoria no .bat de sonarscan

--- src/sonarscan.bat
+++ src/sonarscan.bat
@@ -1,8 +1,8 @@
 dotnet sonarscanner begin /k:"FiapFase1" /d:sonar.host.url="http://localhost:9000"  /d:sonar.token="sqp_c65ca74097e6cb83cf48141072f915825b122f7a" /d:sonar.cs.vscoveragexml.reportsPaths=coverage.xml
 
 
-dotnet build
-dotnet-coverage collect dotnet test -f xml  -o coverage.xml
+dotnet build .\TechChallengeFase1.slnx
+dotnet-coverage collect dotnet test .\TechChallengeFase1.slnx -f xml  -o coverage.xml
 
 dotnet sonarscanner end /d:sonar.token="sqp_c65ca74097e6cb83cf48141072f915825b122f7a"
 
