21ff554b5ca564a7a25b2d6bd1477b3a91c916be
Maieru <soljoaomaieru@hotmail.com>
2026-06-27 10:27:04 -0300

    Separação de ECRs para backend e frontend

--- infra/ecr.tf
+++ infra/ecr.tf
@@ -1,9 +1,19 @@
-resource "aws_ecr_repository" "ecr-repository" {
-  name                 = "fiap-ecr-repository"
+resource "aws_ecr_repository" "ecr-backend-repository" {
+  name                 = "fiap-ecr-backend-repository"
   image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
 
   image_tag_mutability_exclusion_filter {
-    filter      = "latest*"
+    filter      = "*latest"
+    filter_type = "WILDCARD"
+  }
+}
+
+resource "aws_ecr_repository" "ecr-frontend-repository" {
+  name                 = "fiap-ecr-frontend-repository"
+  image_tag_mutability = "IMMUTABLE_WITH_EXCLUSION"
+
+  image_tag_mutability_exclusion_filter {
+    filter      = "*latest"
     filter_type = "WILDCARD"
   }
 }
