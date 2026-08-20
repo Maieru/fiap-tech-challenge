c7149b2f0bda83457c2a813b4e21e3f50b6e7577
Maieru <soljoaomaieru@hotmail.com>
2026-08-20 14:31:45 -0300

    Correções em permissionamento de secrets e reduções em HPAs e números de réplicas

--- infra/kubernetes-addons/remote-state.tf
+++ infra/kubernetes-addons/remote-state.tf
@@ -7,3 +7,13 @@ data "terraform_remote_state" "aws_resources" {
     region = "us-east-1"
   }
 }
+
+data "terraform_remote_state" "database" {
+  backend = "s3"
+
+  config = {
+    bucket = "fiap-s3-terraform-backend"
+    key    = "terraform/database/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
