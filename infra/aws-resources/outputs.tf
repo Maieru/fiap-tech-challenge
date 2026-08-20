3e5dc6cc8ddede0bfb8ef6244f98fde4dc8081b4
Maieru <soljoaomaieru@hotmail.com>
2026-08-20 20:07:20 -0300

    Correção em security group do RDS

--- infra/aws-resources/outputs.tf
+++ infra/aws-resources/outputs.tf
@@ -1,9 +1,10 @@
 output "eks" {
   value = var.create_eks_instance ? {
-    name           = module.eks[0].cluster_name
-    endpoint       = module.eks[0].cluster_endpoint
-    ca             = module.eks[0].cluster_certificate_authority_data
-    security_group = module.eks[0].cluster_security_group_id
+    name                = module.eks[0].cluster_name
+    endpoint            = module.eks[0].cluster_endpoint
+    ca                  = module.eks[0].cluster_certificate_authority_data
+    security_group      = module.eks[0].cluster_security_group_id,
+    node_security_group = module.eks[0].node_security_group_id
   } : null
 }
 
